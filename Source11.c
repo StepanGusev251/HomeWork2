42afd5a98f8c4baf82c5dbecf469236cde9d436f
StepanGusev251 <stepangusev641@gmail.com>
2025-09-22 00:03:47 +0300

    Add files via upload

---
+++ Source11.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <locale.h>
+
+
+int main() {
+	//Объявление и инициализация констант и конкретных переменных
+	const int chislo_chelovek = 120;
+	const int price_vtor_сlass = 200;
+
+	float x = 0.75 ;
+
+	setlocale(LC_ALL, "RUS");
+	//Стоимость билетов второго класса
+	int price_perv_class = price_vtor_сlass * 2;
+	// Количесвто билетов для пассажиров второго класса
+	float Vto_class_tickets = chislo_chelovek * x;
+	// Количесвто пассажиров первого класса
+	float Perv_class_tickets = chislo_chelovek - Vto_class_tickets;
+
+	//Расчёт стоимости билетов превого и второго класса
+	float price_perv = price_perv_class * Perv_class_tickets;
+	float price_vtor = price_vtor_сlass * Vto_class_tickets;
+
+	//Сумма денег от продажи билетов
+	float total_price = price_perv + price_vtor;
+
+	// Форматированный вывод результатов
+	printf("РАСЧЕТ СТОИМОСТИ ВСЕХ БИЛЕТОВ НА РЕЙС \n");
+	printf("================================\n\n");
+	printf("УСЛОВИЯ:\n");
+	printf("Количество всех пассажиров: %d \n", chislo_chelovek);
+	printf("Стоимсоть одного билета второго класса: %d \n", price_vtor_сlass);
+	printf("Стоимсоть одного билета первого класса: %d * 2 = %d \n",price_vtor_сlass, price_perv_class);
+	
+	
+	printf("РАСЧЕТ:\n");
+	printf("Колличество пассажиров второго класса: %d * 0.75 = %.0f \n", chislo_chelovek, Vto_class_tickets);
+	printf("Колличество пассажиров первого класса %d - %.0f = %.0f \n", chislo_chelovek, Vto_class_tickets, Perv_class_tickets);
+	printf("Стоимость билетов второго класса: %d * %.0f = %.0f \n",price_vtor_сlass, Vto_class_tickets, price_vtor);
+	printf("Стоимость билетов первого класса: %d * %.0f = %.0f \n", price_perv_class, Perv_class_tickets, price_perv);
+	printf("Стоимость всех билетов: %.0f \n", total_price);
+
+	return 0;
+}
